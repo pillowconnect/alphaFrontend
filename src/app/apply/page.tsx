94bbcaa7a07ec98084725d0ee1c081a01714a56b
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-12-16 15:05:04 +0530

    Fun

---
+++ src/app/apply/page.tsx
@@ -0,0 +1,8 @@
+
+
+
+export default function Apply() {
+    return (
+        <div className="flex justify-center items-center h-screen text-3xl font-semibold">Dude work in progess :)</div>
+    )
+}
